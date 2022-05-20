bb1bff58bc135412f4bf2b25bab99a24bfac0dd6
4books <joonwoo9192@gmail.com>
2022-05-21 00:45:06 +0900

    MVC Mapping

--- src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -1,14 +1,16 @@
 package hello.springmvc.basic.requestmapping;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Slf4j
 public class MappingController {
 
-    private Logger log = LoggerFactory.getLogger(getClass());
+//    private Logger log = LoggerFactory.getLogger(getClass());
 
     @RequestMapping({"/hello-basic", "/hello-go"})
     public String helloBasic(){
